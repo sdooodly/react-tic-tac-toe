b75c66736c30776bb065ffe8f9918093bbf0f280
Gayathri Gireesh Sujatha <gayathri.sujatha@arup.com>
2024-06-14 16:55:35 +0530

    react tutorial

--- src/App.js
+++ src/App.js
@@ -73,7 +73,7 @@ export default function Game() {
     if (move > 0) {
       description = 'Go to move #' + move;
     } else {
-      description = 'Go to game start';
+      description = 'Resettttttttttttttt';
     }
     return (
       <li key={move}>
